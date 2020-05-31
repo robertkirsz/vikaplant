39efaaef38420a4bd4322debb6db87167b958339
Robert Kirsz <robert.kirsz@gmail.com>
2020-05-31 22:42:55 +0200

    Remove contact form; add map iframe

--- js/main.js
+++ js/main.js
@@ -68,45 +68,4 @@ $(function() {
         prevEffect: 'none',
         nextEffect: 'none'
     });
-
-    //Mapa
-    var googleMap = {
-        stylesArray: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}],
-        start: function() {
-            google.maps.event.addDomListener(window, 'load', this.initialize());
-        },
-        initialize: function() {
-            var mapCanvas = document.getElementById('map-canvas'),
-                wspolrzedne = new google.maps.LatLng(52.3320181, 20.8732535);
-
-            var mapOptions = {
-                center: wspolrzedne,
-                zoom: 15, //0 - 22
-                scrollwheel: false, //Zoom kółkiem myszy
-                mapTypeId: google.maps.MapTypeId.ROADMAP, //ROADMAP, SATELLITE, HYBRID, TERRAIN
-                backgroundColor: '#CECECE',
-                mapTypeControl: true, //Zmiana sposobu wyswietlania - mapa, satelita, teren
-                zoomControlOptions: {
-                    style: google.maps.ZoomControlStyle.SMALL //Mały przycisk zoom
-                },
-                disableDefaultUI: false,
-                styles: this.stylesArray
-            };
-
-            var map = new google.maps.Map(mapCanvas, mapOptions);
-
-            // https://developers.google.com/maps/documentation/javascript/markers#add
-            var marker = new google.maps.Marker({
-                position: wspolrzedne,
-                //icon: '/portfolio/ctrlhome/img/marker.png',
-                map: map
-            });
-
-            $('#map-canvas').click(function() {
-                map.set('scrollwheel', true);
-            });
-        }
-    };
-    googleMap.start();
-
 });
