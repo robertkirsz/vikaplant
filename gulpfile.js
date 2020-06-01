884811e1dc4e4d529903dc3bb319d33795864544
Robert Kirsz <robert.kirsz@gmail.com>
2020-06-01 18:02:24 +0200

    Add browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+const { src, dest, watch } = require('gulp')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
+
+// Static server + watching files
+const serve = () => {
+  browserSync.init({ server: '.' })
+  watch('css/*.scss', css)
+  watch('index.html', browserSync.reload)
+  watch('js/*.js', browserSync.reload)
+}
+
+// Compile SCSS into CSS and auto-inject into browsers
+const css = () =>
+  src('css/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(dest('css/'))
+    .pipe(browserSync.stream())
+
+exports.default = serve
+exports.build = css
--- js/main.js
+++ js/main.js
@@ -1,71 +1,85 @@
-$(function() {
+$(function () {
+  new WOW().init()
 
-    new WOW().init();
+  //Przewijanie strony po kliknięciu na link
+  $('#vika-nawigacja .navbar-nav').on('click', 'a', function (e) {
+    e.preventDefault()
 
-    //Przewijanie strony po kliknięciu na link
-    $('#vika-nawigacja .navbar-nav').on('click', 'a', function(e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - 50
-        }, 500);
-        $(this).parent().siblings().removeClass('active');
-        $(this).parent().addClass('active');
-        //Jeśli strona jest w wersji mobilnej, zamknij pasek z linkami po kliknięciu na któryś z nich
-        if ($(this).parent().parent().parent().hasClass('in')) {
-            $('.navbar-toggle').click();
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top - 50
+      },
+      500
+    )
+
+    $(this).parent().siblings().removeClass('active')
+    $(this).parent().addClass('active')
+
+    //Jeśli strona jest w wersji mobilnej, zamknij pasek z linkami po kliknięciu na któryś z nich
+    if ($(this).parent().parent().parent().hasClass('in')) {
+      $('.navbar-toggle').click()
+    }
+  })
+
+  $('#vika-logo').on('click', function (e) {
+    e.preventDefault()
+    $('html, body').animate({ scrollTop: 0 }, 500)
+  })
+
+  var mainSlider = {
+    $slides: $('#slider .item'),
+    slidesNumber: $('#slider .item').length,
+    start: function () {
+      setInterval(function () {
+        var $aktywny = mainSlider.$slides.filter('.active')
+        if ($aktywny.next().length == 0) {
+          mainSlider.$slides.first().addClass('active')
+        } else {
+          $aktywny.next().addClass('active')
         }
-    });
+        $aktywny.removeClass('active')
+      }, 5000)
+    }
+  }
+  mainSlider.start()
 
-    $('#vika-logo').on('click', function(e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
+  //Wysyłanie wiadomości
+  $('form[name=wiadomosc]').on('submit', function (e) {
+    e.preventDefault()
 
-    var mainSlider = {
-        $slides: $('#slider .item'),
-        slidesNumber: $('#slider .item').length,
-        start: function() {
-            setInterval(function() {
-                var $aktywny = mainSlider.$slides.filter('.active');
-                if ($aktywny.next().length == 0) {
-                    mainSlider.$slides.first().addClass('active');
-                } else {
-                    $aktywny.next().addClass('active');
-                }
-                $aktywny.removeClass('active');
-            }, 5000);
-        }
-    };
-    mainSlider.start();
+    var $this = $(this)
+    var $submit = $(this).find('button[type=submit]')
 
-    //Wysyłanie wiadomości
-    $('form[name=wiadomosc]').on('submit', function(e) {
-        e.preventDefault();
-        var $this = $(this),
-            $submit = $(this).find('button[type=submit]');
-        $.ajax({
-            type: 'POST',
-            url: 'skrypty/wiadomosc.php',
-            data: $this.serialize(),
-            success: function(daneZwrotne) {
-                var json = $.parseJSON(daneZwrotne);
-                //Wyczyść formularz
-                $this.find('textarea').val('');
-                $submit.removeClass('btn-default').addClass('btn-success').html(json.ikona);
-                //Zmień tekst przycisku
-                setTimeout(function() {
-                    $submit.removeClass('btn-success').addClass('btn-default').html('Wyślij');
-                }, 8000);
-            }
-        });
-    });
+    $.ajax({
+      type: 'POST',
+      url: 'skrypty/wiadomosc.php',
+      data: $this.serialize(),
+      success: function (daneZwrotne) {
+        var json = $.parseJSON(daneZwrotne)
+        //Wyczyść formularz
 
-    //Galeria Fancybox
-    $('.fancybox').fancybox({
-        scrollOutside: false,
-        prevEffect: 'none',
-        nextEffect: 'none'
-    });
-});
+        $this.find('textarea').val('')
+
+        $submit
+          .removeClass('btn-default')
+          .addClass('btn-success')
+          .html(json.ikona)
+
+        //Zmień tekst przycisku
+        setTimeout(function () {
+          $submit
+            .removeClass('btn-success')
+            .addClass('btn-default')
+            .html('Wyślij')
+        }, 8000)
+      }
+    })
+  })
+
+  //Galeria Fancybox
+  $('.fancybox').fancybox({
+    scrollOutside: false,
+    prevEffect: 'none',
+    nextEffect: 'none'
+  })
+})
